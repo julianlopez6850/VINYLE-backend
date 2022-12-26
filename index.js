6b2a2682937f79da20023c4066b12d75aa8838ac
Julian Lopez <julianlopez6850@gmail.com>
2022-12-25 22:02:40 -0500

    Added http://localhost:3001 to CORS origin
    - Allows for both ALBUMLE/VINYLE client and Add-Remove-Albums web app to access this API simultaneously.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cors({
 	credentials: true,
-	origin: "http://localhost:3000"
+	origin: ["http://localhost:3000", "http://localhost:3001"]
 }));
 app.use(cookieParser());
 
